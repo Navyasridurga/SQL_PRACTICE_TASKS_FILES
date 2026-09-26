df6a01ab1aafc233c545ba3ccef49de04c56ea94
Navya Sri Durga <kasanavyasridurga@gmail.com>
2026-09-26 16:31:19 +0530

    Refactor EcosystemTask1.sql for clarity and structure
    Removed redundant 'Drop Table Hospital' statement and restructured the creation of the Hospital table. Adjusted insert statement for Hospital table.

--- EcosystemTask1.sql
+++ EcosystemTask1.sql
@@ -2,7 +2,6 @@ create database  Ecosystem;
 drop database Ecosystem;
 use Ecosystem;
 Drop Table Hospital;
-
 create  table employee(
 employeeId int,
 employee_Name text,
@@ -10,14 +9,10 @@ employee_Place text,
 empoye_WorkName text,
 employee_joining_date DATE,
 employee_employeesalary int
-
 );
 select * from employee;
-
 insert into employee values (1001,"sambayya","tadeplligudem","gumasta",'2013-08-14',34000);
 select * from employee;
-
-
 create table student(
 Student_RollNo int,
 Student_Name text,
@@ -25,24 +20,18 @@ Stud_clas_name char,
 Student_Place text
 ); 
 select *from  student;
-
 insert into student values(25,"navya",'8',"ramachandrapuram"); 
 select*from student;
-
 create table amazon(
 product_Name text,
 quantity int,
 rating float,
 order_Place text,
 order_plamazonemployeplace_date DATETIME
-
 );
 select * from amazon;
-
 insert into amazon values("clothes",1,3.9,"secendrabad",'2019-04-13 8:25:30');
 select * from amazon;
-
-
 create table Hospital(
 Hospital_Name varchar(100),
 Doctor_Name varchar(100),
@@ -54,7 +43,6 @@ patient_age int,
 patient_problem varchar(100),
 patient_tablets int,
 patient_joining_date datetime
-
 );
 select * from Hospital;
 insert Hospital values("manipal hospitals dwaraka pvt ltd","Dr. Ch Manoj Kumar" ,
